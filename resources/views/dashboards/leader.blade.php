d0ca538fa162444a3ac3f1b002cd4d06c4e0482f
Michael Lam <agustinus.michael29@gmail.com>
2020-08-21 16:48:51 +0700

    Pushing content on dashboard

---
+++ resources/views/dashboards/leader.blade.php
@@ -0,0 +1,19 @@
+<div class="row mt-5">
+  <div class="col-12 col-xl-12">
+    <div class="row flex-grow">
+      <div class="col-12 grid-margin stretch-card">
+        <div class="card">
+          <div class="card-body">
+            <h6 class="card-title">Format Pengisian File Jadwal</h6>
+            <div class="alert alert-danger" role="alert">
+              <h4 class="alert-heading">Leader harap membaca konten ini karena penting!</h4>
+              <hr>
+              Bagi yang sudah membaca konfirmasi ke Pak Budi. Bila anda bingung, silahkan bertanya ke Michael. Terimakasih.
+            </div>
+            @include('blogs.contents.contoh_format_impor_jadwal')
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
