5bd7a1156250a1e9691dadd8ac3358e218abd199
Michael Lam <agustinus.michael29@gmail.com>
2020-09-26 15:39:55 +0700

    Installing NobleUI

---
+++ resources/views/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="/css/app.css">
+</head>
+<body>
+  A
+  <script src="/js/app.js"></script>
+</body>
+</html>
