48b4c01bacfa7f11116f5f28d86f0afc1ca7a5ea
Michael Lam <agustinus.michael29@gmail.com>
2020-09-29 13:04:11 +0700

    Fix bug on csrf controller

--- app/Http/Controllers/CsrfCookie.php
+++ app/Http/Controllers/CsrfCookie.php
@@ -2,5 +2,5 @@
 
 namespace App\Http\Controllers;
 
-use Laravel\Sanctum\Http\Controllers\CsrfCookieController
+use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
 class CsrfController extends CsrfCookieController {}
