6efdf386ae57ca108847daee4d0087b303e225e1
Michael Lam <agustinus.michael29@gmail.com>
2020-10-09 13:00:36 +0700

    Change the picture algorithm

--- app/Http/Controllers/Gambar.php
+++ app/Http/Controllers/Gambar.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Models\Gambar as GambarModel;
+use Intervention\Image\Facades\Image;
 
 class Gambar extends Controller
 {
   public function index(GambarModel $gambar, Request $request)
   {
-    return response($gambar->konten)->header('Content-Type', 'image/jpeg');
+    $img = Image::make($gambar->konten);
+
+    if ($img->height() > 1000 || $img->width() > 1000)
+      if ($img->height() > $img->width())
+        $img->resize(null, 500, function ($c) {
+          $c->aspectRatio();
+        });
+      else
+        $img->resize(700, null, function ($c) {
+          $c->aspectRatio();
+        });
+
+    return response($img->encode('jpg', 70))->header('Content-Type', 'image/jpeg');
   }
 }
