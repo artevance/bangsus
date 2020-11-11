c06b0fbdc1cb520889b3b9915f0d5951824d7f33
Michael Lam <agustinus.michael29@gmail.com>
2020-11-11 15:39:09 +0700

    Open access for outgoing mutation

--- resources/js/services/access/leader.js
+++ resources/js/services/access/leader.js
@@ -825,7 +825,101 @@ export default {
         read: true,
         update: true,
         delete: true
-      }
+      },
+      outgoingMutation: {
+        access: true,
+        create: true,
+        read: true,
+        detail: true,
+        update: true,
+        approve: true,
+        destroy: true,
+        children: {
+          create: {
+            access: true,
+            timeFree: true,
+            dateMin: 0,
+            dateMax: 0,
+            automatedTime: false,
+            changeSatuan: true
+          },
+          read: {
+            timeFree: true,
+            dateMin: 0,
+            dateMax: 0
+          },
+          detail: {
+            access: true
+          },
+          update: {
+            access: true,
+            timeFree: true,
+            dateMin: 0,
+            dateMax: 0,
+            readonlyTime: false,
+            takePhoto: true,
+            changeSatuan: true
+          },
+          approve: {
+            access: true,
+            timeFree: true,
+            dateMin: 0,
+            dateMax: 0
+          },
+          destroy: {
+            timeFree: true,
+            dateMin: 0,
+            dateMax: 0
+          }
+        }
+      },
+      incomingMutation: {
+        access: true,
+        create: true,
+        read: true,
+        detail: true,
+        update: true,
+        approve: true,
+        destroy: true,
+        children: {
+          create: {
+            access: true,
+            timeFree: true,
+            dateMin: 0,
+            dateMax: 0,
+            automatedTime: false,
+            changeSatuan: true
+          },
+          read: {
+            timeFree: true,
+            dateMin: 0,
+            dateMax: 0
+          },
+          detail: {
+            access: true
+          },
+          update: {
+            access: true,
+            timeFree: true,
+            dateMin: 0,
+            dateMax: 0,
+            readonlyTime: false,
+            takePhoto: true,
+            changeSatuan: true
+          },
+          approve: {
+            access: true,
+            timeFree: true,
+            dateMin: 0,
+            dateMax: 0
+          },
+          destroy: {
+            timeFree: true,
+            dateMin: 0,
+            dateMax: 0
+          }
+        }
+      },
     }
   },
   reportCenter: {
