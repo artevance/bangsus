710150a745874634763fff9e699553a1d5c577ce
Michael Lam <agustinus.michael29@gmail.com>
2020-12-16 14:24:18 +0700

    Fix item bug (5)

--- resources/js/views/form_operasional/stok_opname/Create.vue
+++ resources/js/views/form_operasional/stok_opname/Create.vue
@@ -13,7 +13,7 @@
               <div class="col col-md-6">
                 <div class="form-group">
                   <label>Cabang</label>
-                  <select class="form-control" v-model="form.create.data.cabang_id" @change="selectCabang(form.create.data.cabang_id)" :disabled="form.create.state.cabangReadonly">
+                  <select class="form-control" v-model="form.create.data.cabang_id" @change="selectCabang()" :disabled="form.create.state.cabangReadonly">
                     <option v-for="cabang in data.cabang" :value="cabang.id">
                       {{ cabang.kode_cabang }} - {{ cabang.cabang }}
                     </option>
@@ -148,8 +148,8 @@ export default {
           this.data.cabang = res[0].data.container
         })
     },
-    selectCabang(cabangId) {
-      let tipeCabangId = _.find(this.data.cabang, { 'id': cabangId }).tipe_cabang_id
+    selectCabang() {
+      let tipeCabangId = _.find(this.data.cabang, { 'id': this.form.create.data.cabang_id }).tipe_cabang_id
 
       this.$axios.get('/ajax/v1/master/barang/opname/' + tipeCabangId)
         .then(res => {
