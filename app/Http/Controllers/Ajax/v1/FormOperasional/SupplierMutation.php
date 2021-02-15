6f543da50b2975f8a4b4cacf2628ab7a16445787
artevance <agustinus.michael29@gmail.com>
2021-02-15 11:12:21 +0700

    Adding title to report

--- app/Http/Controllers/Ajax/v1/FormOperasional/SupplierMutation.php
+++ app/Http/Controllers/Ajax/v1/FormOperasional/SupplierMutation.php
@@ -59,6 +59,7 @@ class SupplierMutation extends Controller
     $sheet = $spreadsheet->getActiveSheet();
 
     $container = [
+      ['Mutasi Supplier'],
       ['Cabang', $supplierMutation->cabang->kode_cabang . ' - ' . $supplierMutation->cabang->cabang],
       ['Supplier', $supplierMutation->supplier_mutasi->supplier_mutasi],
       [$supplierMutation->tanggal_form],
