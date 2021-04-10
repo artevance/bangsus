094e4626ac4642282bf0bfcd680fc0b39dc64e28
artevance <agustinus.michael29@gmail.com>
2021-04-10 09:00:07 +0700

    Make the customizable unit based on settings in the property

--- app/Http/Controllers/Ajax/v1/ReportCenter/LaporanKhusus/LaporanPembelian.php
+++ app/Http/Controllers/Ajax/v1/ReportCenter/LaporanKhusus/LaporanPembelian.php
@@ -9,46 +9,145 @@ use App\Http\Models\Barang;
 
 class LaporanPembelian
 {
-  protected $codes = [
-    'BI-00022',
-    'BM-00016',
-    'PR-00022',
-    'BI-00009',
-    'SY-00021',
-    'SY-00010',
-    'SY-00008',
-    'SY-00024',
-    'SY-00022',
-    'SY-00080',
-    'BM-00001',
-    'SY-00132',
-    'SY-00036',
-    'SY-00019',
-    'BM-00052',
-    'BI-00025',
-    'BB-00014',
+  protected $items = [
+    [
+      'kode_barang' => 'BI-00022',
+      'level_satuan' => 2,
+    ],
+    [
+      'kode_barang' => 'BI-00016',
+      'level_satuan' => 2,
+    ],
+    [
+      'kode_barang' => 'PR-00022',
+      'level_satuan' => 1,
+    ],
+    [
+      'kode_barang' => 'BI-00009',
+      'level_satuan' => 2,
+    ],
+    [
+      'kode_barang' => 'SY-00021',
+      'level_satuan' => 2,
+    ],
+    [
+      'kode_barang' => 'SY-00010',
+      'level_satuan' => 2,
+    ],
+    [
+      'kode_barang' => 'SY-00008',
+      'level_satuan' => 2,
+    ],
+    [
+      'kode_barang' => 'SY-00024',
+      'level_satuan' => 2,
+    ],
+    [
+      'kode_barang' => 'SY-00022',
+      'level_satuan' => 2,
+    ],
+    [
+      'kode_barang' => 'SY-00080',
+      'level_satuan' => 2,
+    ],
+    [
+      'kode_barang' => 'BM-00001',
+      'level_satuan' => 2,
+    ],
+    [
+      'kode_barang' => 'SY-00132',
+      'level_satuan' => 2,
+    ],
+    [
+      'kode_barang' => 'SY-00036',
+      'level_satuan' => 2,
+    ],
+    [
+      'kode_barang' => 'SY-00019',
+      'level_satuan' => 2,
+    ],
+    [
+      'kode_barang' => 'BM-00052',
+      'level_satuan' => 2,
+    ],
+    [
+      'kode_barang' => 'BI-00025',
+      'level_satuan' => 1,
+    ],
+    [
+      'kode_barang' => 'BB-00014',
+      'level_satuan' => 2,
+    ],
+    [
+      'kode_barang' => 'BM-00009',
+      'level_satuan' => 1,
+    ],
   ];
 
   public function index(Request $request)
   {
     $tanggal = $request->query('tanggal');
     $pembelian = Pembelian::whereDate('dTgl', '=', $tanggal)
-      ->whereIn('cKdBrg', $this->codes)
+      ->whereIn('cKdBrg', collect($this->items)->pluck('kode_barang')->all())
       ->get();
     $pembelian->each(function ($p) {
       $barang = Barang::where('kode_barang', $p->cKdBrg)->first();
+      $levelSatuan = collect($this->items)->where('kode_barang', $p->cKdBrg)
+        ->min('level_satuan');
+      $levelPembelian = 0;
+      switch (true) {
+        case $p->nqty1 != 0:
+          $levelPembelian = 1;
+        case $p->nqty2 != 0:
+          $levelPembelian = 2;
+        case $p->nqty3 != 0:
+          $levelPembelian = 3;
+        case $p->nqty4 != 0:
+          $levelPembelian = 4;
+        case $p->nqty5 != 0:
+          $levelPembelian = 5;
+      }
 
-      if ($p->nqty2 != 0) {
-        $p->nHarga1 /= $barang->rasio_dua ?? 1;
-      }
-      if ($p->nqty3 != 0) {
-        $p->nHarga1 /= $barang->rasio_tiga ?? 1;
-      }
-      if ($p->nqty4 != 0) {
-        $p->nHarga1 /= $barang->rasio_empat ?? 1;
-      }
-      if ($p->nqty5 != 0) {
-        $p->nHarga1 /= $barang->rasio_lima ?? 1;
+      if ($levelSatuan != $levelPembelian) {
+        if ($levelSatuan > $levelPembelian) {
+          for ($i = $levelPembelian; $i <= $levelSatuan; $i++) {
+            $rasio = 1;
+            switch ($i) {
+              case 2:
+                $rasio = $barang->rasio_dua ?? 1;
+                break;
+              case 3:
+                $rasio = $barang->rasio_tiga ?? 1;
+                break;
+              case 4:
+                $rasio = $barang->rasio_empat ?? 1;
+                break;
+              case 5:
+                $rasio = $barang->rasio_lima ?? 1;
+                break;
+            }
+            $p->nHarga1 *= $rasio;
+          }
+        } else {
+          for ($i = $levelSatuan; $i <= $levelPembelian; $i++) {
+            $rasio = 1;
+            switch ($i) {
+              case 2:
+                $rasio = $barang->rasio_dua ?? 1;
+                break;
+              case 3:
+                $rasio = $barang->rasio_tiga ?? 1;
+                break;
+              case 4:
+                $rasio = $barang->rasio_empat ?? 1;
+                break;
+              case 5:
+                $rasio = $barang->rasio_lima ?? 1;
+                break;
+            }
+            $p->nHarga1 /= $rasio;
+          }
+        }
       }
     });
 
